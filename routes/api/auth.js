bc261bc0638d25755a66cc4a8bf97493c8875bb9
Muhammed ogunsanya <mspeaks910@gmail.com>
2019-11-05 13:23:13 +0100

    created auth middleware for access to proctected routes

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,17 @@
 const express = require('express');
-
+const auth = require('../../middleware/auth');
+const User = require ('../../model/User')
 const router = express.Router();
 
-router.get('/', (req,res) => {
-    res.send('Welcome to auth API');
+router.get('/', auth, async(req,res) => {
+    try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+    }
+    catch(err) {
+     console.log(err.message);
+     res.status(500).send('Internal server error');
+    }
 })
 
 module.exports = router;
