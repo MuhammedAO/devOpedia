617f6d5a6eb3384a4fec1565f57ebde86703b52b
Muhammed ogunsanya <mspeaks910@gmail.com>
2019-11-07 17:46:24 +0100

    Get all profiles & get profile by UID

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -89,7 +89,38 @@ router.post('/', [auth , [
       console.log(err.message);
       res.status(500).send('Server Error');
     }
-}
-);
+});
+
+
+//Get all profiles
+router.get('/', async (req,res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+    res.json(profiles); 
+  } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+  }
+});
+
+//Get profile by UID
+router.get('/user/:user_id', async (req,res) => {
+    try {
+      const profile = await Profile
+      .findOne({user:req.params.user_id})
+      .populate('user', ['name', 'avatar']);
+
+      if(!profile) return res.status(400).json({msg:'Profile not found'});
+
+      res.json(profile); 
+
+    } catch (err) {
+        console.error(err.message);
+        if(err.kind == 'ObjectId') {
+        return res.status(400).json({msg:'Profile not found'});
+        }
+        res.status(500).send('Server Error');
+    }
+  });
 
 module.exports = router;
