59736f65641a94ea146d8695f039c2f9e7babde8
Muhammed ogunsanya <mspeaks910@gmail.com>
2019-11-11 13:14:32 +0100

    redux store and reducers setup

---
+++ client/src/store.js
@@ -0,0 +1,11 @@
+import {createStore, applyMiddleware, compose} from 'redux'
+import thunk from 'redux-thunk'
+import rootReducer from './reducers/index'
+
+const initState = {};
+
+const middleware = [thunk];
+
+const store = createStore(rootReducer, initState, compose(applyMiddleware(...middleware)));
+
+export default store
