fa5c062992dfbb7bf2f9d096ad0458c8ede53429
Muhammed ogunsanya <mspeaks910@gmail.com>
2019-11-17 23:23:44 +0100

    Display/List Education and experience on the dashboard

--- client/src/components/profile-forms/AddExperience.js
+++ client/src/components/profile-forms/AddExperience.js
@@ -83,7 +83,4 @@ AddExperience.propTypes = {
 addExperience: PropTypes.func.isRequired
 }
 
-export default connect(null,
-{ addExperience}
-)
-(AddExperience);
+export default connect(null,{ addExperience})(AddExperience);
