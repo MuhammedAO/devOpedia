1a6f594314bcef058017efac8be4b9eef6164ae6
Muhammed ogunsanya <mspeaks910@gmail.com>
2019-11-21 14:11:17 +0100

    PostItem Component

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -3,16 +3,26 @@ import {connect} from 'react-redux'
 import PropTypes from 'prop-types'
 import {getPosts} from '../../actions/post'
 import Spinner from '../layout/Spinner'
-const Posts = ({getPosts, post:{post, loading}}) => {
+import PostItem from './PostItem'
+const Posts = ({getPosts, post:{posts, loading}}) => {
     useEffect(() => {
         getPosts();
     }, [getPosts]);
 
 
-    return (
-        <Fragment>
-            HEY
-        </Fragment>
+    return loading ? <Spinner/> : (
+
+    <Fragment>
+    <h1 className="large text-primary">Posts</h1>
+    <p className="lead">
+        <i className="fas fa-user"></i> Welcome To The Community
+    </p>
+    <div className="posts">
+        {posts.map(post => (
+            <PostItem key={post._id} post={post}/>
+        ))}
+    </div>
+    </Fragment>
     )
 }
 
