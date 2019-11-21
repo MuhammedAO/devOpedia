f4ba444e9a769951830b24eeb0a80487c7aa3616
Muhammed ogunsanya <mspeaks910@gmail.com>
2019-11-21 23:43:09 +0100

    Adding Comments

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -5,6 +5,7 @@ import {Link} from 'react-router-dom'
 import Spinner from '../layout/Spinner'
 import {getPost} from '../../actions/post'
 import PostItem from '../posts/PostItem'
+import CommentForm from './CommentForm'
 const Post = ({getPost, post:{post, loading}, match}) => {
     useEffect(() => {
         getPost(match.params.id);
@@ -14,6 +15,7 @@ const Post = ({getPost, post:{post, loading}, match}) => {
     <Fragment>
         <Link to="/posts" className="btn">Back To Posts</Link>
         <PostItem post={post} showActions={false}/>
+        <CommentForm postId={post._id}/>
     </Fragment>
   
 }
